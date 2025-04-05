b02d8a13ad35ec7bb7a53df2aaf3070cf4e5f86a
prateekrauniyar345 <raun9583@vandals.uidaho.edu>
2025-04-04 22:20:58 -0700

    this is fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// this is main file. 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
